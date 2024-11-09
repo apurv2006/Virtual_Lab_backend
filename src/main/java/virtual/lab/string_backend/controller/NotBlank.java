8bb12fdae41e477fd6c682e03ff6a3696ac2ab28
apurv2006 <ecs.crce.10125@gmail.com>
2024-11-09 23:31:42 +0530

    Working code

--- src/main/java/virtual/lab/string_backend/controller/NotBlank.java
+++ src/main/java/virtual/lab/string_backend/controller/NotBlank.java
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 package virtual.lab.string_backend.controller;
 
-public @interface NotBlank {
-
+public class NotBlank {
+    // Your class content here
 }
-=======
-package virtual.lab.string_backend.controller;
-
-public @interface NotBlank {
-
-}
->>>>>>> 0f7690443f4f25968eaad89edc135e90b05e7fdb
